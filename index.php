6f01d3b8ce74a0cdc5ddcbcc0cf4c3df18de712a
Weirdo1_1 <samyjabarti@hotmail.com>
2023-11-11 19:40:11 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+<form action="index.php">
+
+<input type="text" name="firstname" id="firstname">
+<input type="text" name="lastname" id="lastname">
+<input type="text" name="email" id="email">
+firstname
+</form>
+
+    
+<script src="./js/script.js"></script>
+
+</body>
+</html>
+
